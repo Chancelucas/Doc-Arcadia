25466b405614c1a30619177593123b14531ec5da
chancelucas <lucas.chancel@laposte.net>
2024-01-25 09:55:18 +0100

    Add nav-bar page

---
+++ Back-end/component/nav-bar.php
@@ -0,0 +1,8 @@
+
+<nav>
+    <ul>
+        <li><a href="../../Front-end/index.php"></a>Accueil</li>
+        <li><a href="../../Front-end/Pages/plan.php">Schéma</a></li>
+        <li><a href="../../Front-end/Pages/contact.php"></a>Contact</li>
+    </ul>
+</nav>
